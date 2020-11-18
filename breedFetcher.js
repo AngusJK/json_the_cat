eb1a89b7157a55b5b8a4ef1afd082c5707d8690c
Angus Kellett <anguskellett@hotmail.com>
2020-11-18 17:20:16 +0000

    passes result into callback; updates url call

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,43 @@
-const args = process.argv.slice(2);
-
+//const args = process.argv.slice(2);
+const fs = require('fs');
 const request = require('request');
 
-let urlStart = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let urlComplete = urlStart.concat(args[0]);
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    
+    const data = JSON.parse(body);
+    const desc = data[0]['description'];
 
-request(urlComplete, (error, response, body) => {
-  if (error !== null) {
-    console.log("Something went wrong. Try again later.");
-    return;
-  }
-  console.log('error: ', error);
-  console.log('status: ', response && response.statusCode);
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Sorry. This breed does not appear in our database.");
-    return;
-  }
-  console.log(data[0]['description']);
-});
+    if (!error) callback(null, desc);
+
+    
+    
+
+  
+    //return 'error: ', error;
+    //console.log('status: ', response && response.statusCode);
+    
+    //if (data[0] === undefined) {
+      //return "Sorry. This breed does not appear in our database.";
+    //}
+    //console.log(data[0]['description']);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+/*
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./datum/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+module.exports = breedDetailsFromFile;
+
+*/
