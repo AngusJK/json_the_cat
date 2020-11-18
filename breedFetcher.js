5695afeba58aaf68c374aff204392d003c2ab18b
Angus Kellett <anguskellett@hotmail.com>
2020-11-18 05:26:30 +0000

    uses user input to create url; returns error messages

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,21 @@
-const request = require('request');
-//const fs = require('fs');
+const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+const request = require('request');
+
+let urlStart = 'https://api.thecatapi.com/v1/breeds/search?q=';
+let urlComplete = urlStart.concat(args[0]);
+
+request(urlComplete, (error, response, body) => {
+  if (error !== null) {
+    console.log("Something went wrong. Try again later.");
+    return;
+  }
   console.log('error: ', error);
   console.log('status: ', response && response.statusCode);
-  console.log("body: ", body);
+  const data = JSON.parse(body);
+  if (data[0] === undefined) {
+    console.log("Sorry. This breed does not appear in our database.");
+    return;
+  }
+  console.log(data[0]['description']);
 });
